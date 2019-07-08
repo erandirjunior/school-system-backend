38bdcfc85e1feffde6d93159670b55bb173d1035
Erandir <aefs12junior@gmail.com>
2019-07-08 20:55:52 -0300

    Created category domain

---
+++ src/Domain/Category/CategoryCreatorService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace School\Domain\Category;
+
+use Doctrine\ORM\EntityManager;
+use School\Infrastructure\Domain\Repository\CategoryRepository;
+use School\Infrastructure\Domain\Service\Service;
+
+class CategoryCreatorService extends Service
+{
+	private $repository;
+
+	private $validator;
+
+	public function __construct(CategoryRepository $repository,
+								CategoryValidator $categoryValidator)
+	{
+		$this->repository 	= $repository;
+		$this->validator	= $categoryValidator;
+	}
+
+	public function create()
+	{
+		$category = new Category();
+		$category->setName('Student');
+
+		$this->repository->persist($category);
+		$this->repository->flush();
+
+		/*$this->repository->persist($category);
+		$this->repository->flush();*/
+	}
+
+	public function update()
+	{
+
+    }
+
+	public function delete($id)
+	{
+
+    }
+
+	public function index()
+	{
+
+    }
+}
